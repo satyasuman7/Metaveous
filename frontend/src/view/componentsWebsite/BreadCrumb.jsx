dabe36106dde2cd8d86cc6d3ad67c64d456b4865
satyasuman7 <satyasuman9893@hotmail.com>
2025-11-20 13:22:43 +0530

    changes some bootstrap in BreadCrumb component

--- frontend/src/view/componentsWebsite/BreadCrumb.jsx
+++ frontend/src/view/componentsWebsite/BreadCrumb.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function BreadCrumb({ title }) {
   return (
     <div className="container-fluid breadcrumb_card text-center text-white">
-      <h1 className="fw-bold mb-3">{title}</h1>
+      <h1 className="fw-bold mb-3 text-uppercase">{title}</h1>
       <nav aria-label="breadcrumb">
         <ol className="breadcrumb justify-content-center">  
           <li className="breadcrumb-item">
